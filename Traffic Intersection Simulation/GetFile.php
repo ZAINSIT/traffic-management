e27595f894de2662b37a21d49644380767baf2e2
ZAINSIT <zainzayu250@gmail.com>
2025-07-20 21:12:12 +0530

    Add files via upload

---
+++ Traffic Intersection Simulation/GetFile.php
@@ -0,0 +1,68 @@
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="icon" type="image/png" href="/favicon.png?v=1">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB"
+        crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="/css/styles.css?v=7" />
+    
+    
+    <title>VidsWatch</title>
+</head>
+
+<body>
+
+    <nav class="c-nav-bar navbar navbar-expand-md navbar-dark ">
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+            aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="/">HOME
+                                                    <span class="sr-only">(current)</span>
+                                            </a>
+                </li>
+
+                <li class="nav-item ">
+                    <a class="nav-link" href="/terms.php">TERMS
+                        </a>
+                </li>
+                <li class="nav-item ">
+                    <a class="nav-link" href="/contact.php">CONTACT
+                        </a>
+                </li>
+
+
+
+            </ul>
+        </div>
+    </nav>
+    <div class="container c-main-content">
+
+        <div class="row  logo-row">
+            <div class="col-sm c-logo-container">
+                <img class="c-logo img-fluid" src="img/logo_new.png" alt="Logo" />
+            </div>
+
+        </div>
+        <div class="c-main-content-container">
+<section class="c-content-section">
+	<h1>Uh oh! An error has occured</h1>
+	<p class="mt-2">
+		An unexpected error has occured while trying to process the requested page. Our engineers have been notified and working to fix it as soon as possible.
+		<button class="btn btn-primary" onclick="history.back()"><i class="fa fa-refresh"></i> Try again</button>
+
+	</p>
+</section>
